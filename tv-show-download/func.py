af8ca6a3e9ec2719ef230992c4d87a0364cd814f
AutoTest <fedssedov@gmail.com>
2021-02-12 17:37:31 +0300

    A little of PEP8 formatting

--- tv-show-download/func.py
+++ tv-show-download/func.py
@@ -8,9 +8,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+
 def download_best_quality(driver, element_list):
     download_links = element_list[1].find_elements_by_xpath("//ul[@class='tlsiconkoi']/li")
     download_links[len(download_links)-1].click()
+
 
 def wait_season_page_load_and_gather_list(driver):
     try:
@@ -20,8 +22,10 @@ def wait_season_page_load_and_gather_list(driver):
     except Exception as inst:
         print("Exception in function wait_season_page_load_and_gather_list. Error loading Show page")
         print(inst)
+
     season_list = season.find_elements_by_xpath("//ul[@class='tl2']/li")
     return season_list
+
 
 def previous_page(driver):
     try:
@@ -31,15 +35,18 @@ def previous_page(driver):
     except Exception as inst:
         print("Exception in function previous_page. Cant find back button")
         print(inst)
+
     button = driver.find_element_by_xpath("//img[@src='/style/img/vernutca.png']")
     button.click()
+
 
 def define_opened_page_go_to_seasons_page(driver):
     if "anwap.bio/serials/comm/" in driver.current_url:
         parsed_url = driver.current_url.split("/")
         parsed_url.remove('comm')
-        if parsed_url[-1].isdigit() and parsed_url[-2].isdigit(): #check if we are not on 1st page of comments
-            del parsed_url[-1] #delete page id from ult
+        if parsed_url[-1].isdigit() and parsed_url[-2].isdigit():  # Сheck if we are not on 1st page of comments
+            del parsed_url[-1]  # Delete page id from ult
+
         target_url = '/'.join(parsed_url)
         driver.get(target_url)
         driver.get(target_url)
@@ -51,16 +58,19 @@ def define_opened_page_go_to_seasons_page(driver):
         print("Exception in function define_opened_page_go_to_seasons_page. Couldn't find elemebt on page. Passable")
         print(inst)
         pass
-    if len(blm_check)==1:
+
+    if len(blm_check)==1:  # Go to shows page
         previous_page(driver)
     else:
         for i in range(2):
             previous_page(driver)
 
+
 def find_and_click_search_results(driver, main):
     results = main.find_elements_by_class_name("tF2Cxc")
     link = results[0].find_element_by_class_name("yuRUbf")
     link.click()
+
 
 def search_and_wait_results(driver, show):
     search = driver.find_element_by_name("q")
@@ -76,16 +86,19 @@ def search_and_wait_results(driver, show):
         print("Exception in function search_and_wait_results. Google page didnt open in time")
         print(inst)
 
+
 def close_adblock(driver):
     curWindowHndl = driver.current_window_handle
     driver.switch_to_window(driver.window_handles[1])
     driver.close()
     driver.switch_to_window(curWindowHndl)
 
+
 def wait_downloads_to_finish(driver):
     WebDriverWait(driver, 300, 1).until(every_downloads_chrome(driver))
 
-def every_downloads_chrome(driver): # Функция по ожиданию окончания всех загрузок
+
+def every_downloads_chrome(driver):  #  Function to wait for all downloads to finish
     if not driver.current_url.startswith("chrome://downloads"):
         driver.get("chrome://downloads/")
     return driver.execute_script("""
@@ -95,7 +108,8 @@ def every_downloads_chrome(driver): # Функция по ожиданию ок�
             return items.map(e => e.fileUrl || e.file_url);
         """)
 
-def episode_loop(driver): # Скачиваем все серии начиная с 1
+
+def episode_loop(driver):  # Download al episodes starting from 1
     a=1
     while a == 1:
         try:
@@ -106,6 +120,7 @@ def episode_loop(driver): # Скачиваем все серии начиная 
         except Exception as inst:
             print("Exception in function episode_loop. Couldn't load page")
             print(inst)
+
         if "Следующая" in head.text:
             download_best_quality(driver, element_list)
             head_next = head.find_element_by_xpath("//img[@src='/style/img/sright.png']")
@@ -117,7 +132,7 @@ def episode_loop(driver): # Скачиваем все серии начиная 
                 previous_page(driver)
         
 
-def start_download_process(driver): # Начинаем процесс скачивания
+def start_download_process(driver): 
     season_list = wait_season_page_load_and_gather_list(driver)
     current_season = 0
     overall_seasons = len(season_list)
@@ -128,7 +143,7 @@ def start_download_process(driver): # Начинаем процесс скачи
         current_season = current_season + 1
     
 
-def download_season(driver): #Запуск процесса скачивания первой серии
+def download_season(driver): 
     try:
         list = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "blm"))
