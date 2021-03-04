3f6a97b54995062a291c284bfa16ad6d6c6ac955
AutoTest <fedssedov@gmail.com>
2021-03-04 19:02:08 +0300

    Nested classes and second wibsite for downloading

--- tv-show-download/tv_show_download.py
+++ tv-show-download/tv_show_download.py
@@ -1,42 +1,37 @@
-
-from selenium import webdriver
-import warnings
-import threading
-from selenium.webdriver.chrome.options import Options
 import func
-import globals
 
-path_to_extension = r'C:\Program Files (x86)\3.10.1_0'
-chrome_options = Options()
-chrome_options.headless = True
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-
-# Hide browser errors from console
-chrome_options.add_argument("--log-level=3")
-chrome_options.add_argument('load-extension=' + path_to_extension)
-driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
-driver.create_options()
-warnings.filterwarnings("ignore") # Ignore warnings
-
-driver.get("https://google.com")
 print("Введите название сериала")
 show = input()
-main = func.search_and_wait_results(driver, show)
-func.find_and_click_search_results(driver, main)
-func.define_opened_page_go_to_seasons_page(driver)
-func.start_download_process(driver)
 
+print("Какой сайт выбрать для скачки ( 1-anwap | 2-zetflix ) ")
+site = input()
 
-
+downloader = func.show_downloader()
+if int(site) == 1:
+    anwap_downloader = downloader.show_downloader_anwap(show)
+    main = anwap_downloader.search_and_wait_results()
+    anwap_downloader.find_and_click_search_results(main)
+    anwap_downloader.define_opened_page_go_to_seasons_page()
+    anwap_downloader.start_download_process()
+    downloader.episode_urls = anwap_downloader.return_urls()
+    downloader.episode_names = anwap_downloader.return_names()
+    driver = anwap_downloader.driver
+else:
+    zetflix_downloader = downloader.show_downloader_zetflix(show)
+    zetflix_downloader.open_site()
+    zetflix_downloader.find_searchbar()
+    zetflix_downloader.click_show()
+    zetflix_downloader.prepare_download_links()
+    downloader.episode_urls = zetflix_downloader.return_urls()
+    downloader.episode_names = zetflix_downloader.return_names()
+    driver = zetflix_downloader.driver
 try:
-    func.create_threads()
-    #print(threading.enumerate())
-    func.queue_put()
-    globals.q.join()
-    #tag
+    downloader.create_threads()
+    downloader.queue_put()
+    downloader.q.join()
     print("Exited Queue")
-    func.queue_empty()
-    func.join_threads()
+    downloader.queue_empty()
+    downloader.join_threads()
 except Exception as inst:
         print("Exception during download process")
         print(inst)
@@ -44,5 +39,3 @@ else:
     print("JOB DONE!!!!!!!!!!!!!!!!!!!!!!!")
 finally:
     driver.quit()
-
-#trying to fix git
